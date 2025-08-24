653c550baf88af1e2f8ebecfacc6c5ba875cabbe
unknown <viktorijka20@gmail.com>
2025-08-24 16:32:51 +0300

    add modified files

--- src/main.js
+++ src/main.js
@@ -6,27 +6,29 @@ import * as rendered from './js/render-functions';
 
 const closeSVGLink = new URL('./img/x-octagon.svg', import.meta.url).href;
 const form = document.querySelector('.form');
+const loadMoreBtn = document.querySelector('.js-load-more');
+
 let meaning = '';
+let page = 1;
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   meaning = form.elements['search-text'].value.trim();
-  if (meaning === '') {
+  if (!meaning) {
     return iziToast.warning({
       backgroundColor: 'orange',
-      message:
-        'Sorry, there is nothing provided here to look for. Please try again!',
+      message: 'Please enter a search term!',
       messageColor: '#fafafa',
-      messageSize: '16px',
-      messageLineHeight: 1.5,
       position: 'topLeft',
     });
   }
 
+  page = 1;
   rendered.clearGallery();
+  rendered.hideLoadMoreButton();
   rendered.showLoader();
 
-  getImagesByQuery(meaning)
+  getImagesByQuery(meaning, page)
     .then(reply => {
       rendered.hideLoader();
       form.elements['search-text'].value = '';
@@ -34,19 +36,42 @@ form.addEventListener('submit', e => {
         return iziToast.error({
           backgroundColor: '#ef4040',
           class: 'error-message',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
+          message: 'No images found for your query.',
           messageColor: '#fafafb',
-          messageSize: '16px',
-          messageLineHeight: 1.5,
-          position: 'topRight',
           iconUrl: closeSVGLink,
         });
       }
 
       rendered.createGallery(reply.hits);
+
+      if (reply.totalHits > page * 15) {
+        rendered.showLoadMoreButton();
+      }
     })
-    .catch(error => {
-      console.log(error);
-    });
+    .catch(error => console.log(error));
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  page += 1;
+  rendered.hideLoadMoreButton();
+  rendered.showLoader();
+
+  try {
+    const reply = await getImagesByQuery(meaning, page);
+    rendered.hideLoader();
+
+    rendered.createGallery(reply.hits);
+
+    if (reply.totalHits > page * 15) {
+      rendered.showLoadMoreButton();
+    } else {
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+    }
+  } catch (error) {
+    rendered.hideLoader();
+    console.log(error);
+  }
 });
